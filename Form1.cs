39a0b58ab32f1df7008bc9bbe78a97bb0fc138e5
alexesp <alexizaes@hotmail.com>
2025-04-09 08:16:23 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLC_Simulador_de_Tren_Tia_Portal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
